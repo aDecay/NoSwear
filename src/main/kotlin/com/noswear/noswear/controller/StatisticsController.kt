a3b0507828b0bfe6f1ec1a39a54c2a45daff6579
adecay <younghyeon55@gmail.com>
2024-04-28 17:01:12 +0900

    feat: edit response type of get statistics

--- src/main/kotlin/com/noswear/noswear/controller/StatisticsController.kt
+++ src/main/kotlin/com/noswear/noswear/controller/StatisticsController.kt
@@ -8,8 +8,8 @@ class StatisticsController {
     @GetMapping("/statistics")
     fun statistics(): List<Map<String, Any>> {
         val result = mutableListOf<Map<String, Any>>()
-        result.add(mutableMapOf<String, Any>("test1" to 5))
-        result.add(mutableMapOf<String, Any>("test2" to 10))
+        result.add(mutableMapOf<String, Any>("word" to "test1", "frequency" to 5))
+        result.add(mutableMapOf<String, Any>("word" to "test2", "frequency" to 10))
         return result
     }
 }
