9289d20ea6c043536c002fa4329795bd468d853c
adecay <younghyeon55@gmail.com>
2024-05-01 14:45:49 +0900

    feat: returns all data in frequency table as List of StatisticsDto when get statistics requested

--- src/main/kotlin/com/noswear/noswear/controller/StatisticsController.kt
+++ src/main/kotlin/com/noswear/noswear/controller/StatisticsController.kt
@@ -1,15 +1,16 @@
 package com.noswear.noswear.controller
 
+import com.noswear.noswear.dto.StatisticsDto
+import com.noswear.noswear.service.StatisticsService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class StatisticsController {
+class StatisticsController(
+    private val statisticsService: StatisticsService
+) {
     @GetMapping("/statistics")
-    fun statistics(): List<Map<String, Any>> {
-        val result = mutableListOf<Map<String, Any>>()
-        result.add(mutableMapOf<String, Any>("word" to "test1", "frequency" to 5))
-        result.add(mutableMapOf<String, Any>("word" to "test2", "frequency" to 10))
-        return result
+    fun statistics(): List<StatisticsDto> {
+        return statisticsService.findAll()
     }
 }
