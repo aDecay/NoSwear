fb59c36501a48cce1fd18158640b14aedd55a415
adecay <younghyeon55@gmail.com>
2024-05-03 00:18:09 +0900

    feat: get speech-to-text result of file

--- src/main/kotlin/com/noswear/noswear/service/SendService.kt
+++ src/main/kotlin/com/noswear/noswear/service/SendService.kt
@@ -1,9 +1,23 @@
 package com.noswear.noswear.service
 
 import com.noswear.noswear.repository.FrequencyRepository
+import mu.KotlinLogging
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
+private val logger = KotlinLogging.logger {}
+
+@Service
 class SendService (
     private val frequencyRepository: FrequencyRepository
 ) {
-
+    fun processFile(filename: String) {
+        val processBuilder = ProcessBuilder("whisper", filename, "--language", "Korean")
+        val process = processBuilder.start()
+        BufferedReader(InputStreamReader(process.inputStream))
+            .forEachLine { line ->
+                logger.debug(line)
+            }
+    }
 }
