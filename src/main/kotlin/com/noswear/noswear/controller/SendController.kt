743b6ba79de1dbbc59c213a0a3d9f2cd5f93b1ab
adecay <younghyeon55@gmail.com>
2024-05-02 14:49:31 +0900

    feat: increment frequency if id exists, else create new record with initializing frequency to 0

--- src/main/kotlin/com/noswear/noswear/controller/SendController.kt
+++ src/main/kotlin/com/noswear/noswear/controller/SendController.kt
@@ -1,17 +1,35 @@
 package com.noswear.noswear.controller
 
+import com.noswear.noswear.domain.Frequency
+import com.noswear.noswear.domain.FrequencyId
 import com.noswear.noswear.dto.RecordDto
+import com.noswear.noswear.repository.FrequencyRepository
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
-class SendController {
+class SendController (
+    private val statisticsRepository: FrequencyRepository
+) {
     @PostMapping("/send")
-    fun send(@RequestBody record: RecordDto) {
+    fun send(@RequestBody record: RecordDto): Frequency {
+        val date = LocalDate.now().toString()
+        val frequencyId = FrequencyId(date, date)
+
         val data = record.data
         for (x in data) {
             print(x)
         }
+        println()
+
+        return statisticsRepository.findById(frequencyId)
+            .map { frequency ->
+                frequency.frequency += 1
+                return@map statisticsRepository.save(frequency)
+            }.orElseGet {
+                statisticsRepository.save(Frequency(frequencyId, 0))
+            }
     }
 }
