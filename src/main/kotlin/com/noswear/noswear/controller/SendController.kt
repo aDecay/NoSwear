361ceb4ee5d893d1a387212f933f91237e62d3f0
adecay <younghyeon55@gmail.com>
2024-05-06 01:39:36 +0900

    feat: analyze sent files and save to db

--- src/main/kotlin/com/noswear/noswear/controller/SendController.kt
+++ src/main/kotlin/com/noswear/noswear/controller/SendController.kt
@@ -1,36 +1,18 @@
 package com.noswear.noswear.controller
 
-import com.noswear.noswear.domain.Frequency
-import com.noswear.noswear.domain.FrequencyId
 import com.noswear.noswear.dto.RecordDto
-import com.noswear.noswear.repository.FrequencyRepository
-import mu.KotlinLogging
+import com.noswear.noswear.service.SendService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
-
-private val logger = KotlinLogging.logger {}
+import java.util.*
 
 @RestController
 class SendController (
-    private val statisticsRepository: FrequencyRepository
+    private val sendService: SendService
 ) {
     @PostMapping("/send")
-    fun send(@RequestBody record: RecordDto): Frequency {
-        val date = LocalDate.now().toString()
-        val frequencyId = FrequencyId(date, date)
-
-        val data = record.data
-        val length = data.length
-        logger.debug("length: ${length}, prefix: ${if (length < 10) length else 10}")
-
-        return statisticsRepository.findById(frequencyId)
-            .map { frequency ->
-                frequency.frequency += 1
-                return@map statisticsRepository.save(frequency)
-            }.orElseGet {
-                statisticsRepository.save(Frequency(frequencyId, 0))
-            }
+    fun send(@RequestBody record: RecordDto) {
+        sendService.analyze(record.data)
     }
 }
