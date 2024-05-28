88fbae8b0e1bd7bf842a6ec3e8b5df42ea4f9e6d
adecay <younghyeon55@gmail.com>
2024-05-28 16:17:52 +0900

    feat: also returns user id in user response

--- src/main/kotlin/com/noswear/noswear/dto/UserResponse.kt
+++ src/main/kotlin/com/noswear/noswear/dto/UserResponse.kt
@@ -3,12 +3,14 @@ package com.noswear.noswear.dto
 import com.noswear.noswear.domain.User
 
 data class UserResponse(
+    val id: Int,
     val name: String,
     val email: String
 ) {
     companion object {
         fun of(user: User): UserResponse {
             return UserResponse(
+                user.id!!,
                 user.name,
                 user.email
             )
